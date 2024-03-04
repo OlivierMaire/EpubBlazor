f927ba09b8544683244210fc2964f6a872d53b9e
Olivier <olivier.maire.jp@gmail.com>
2024-03-05 00:19:12 +0900

    handle multiple pages per chapter

--- Models/Position.cs
+++ Models/Position.cs
@@ -3,26 +3,30 @@ using System.Globalization;
 
 namespace EPubBlazor.Models;
 
-public record Position(int[] NavigationItemIndex, decimal ScrollPosition)
+public record Position(string NavigationLevel, int ReadOrder, decimal ScrollPosition)
 {
+    public string NavigationLevel {get;set;} = NavigationLevel;
+    public int ReadOrder {get;set;} = ReadOrder;
     public decimal ScrollPosition {get;set;} = ScrollPosition;
+ 
 
     public static Position Parse(string input)
     {
-        var match = Regex.Match(input, @"^\((?:,?([0-9]+))+\|(.*)\)$");
-        if (match != null && match.Success && match.Groups != null && match.Groups.Count == 3)
+        var match = Regex.Match(input, @"^\(((?:[0-9]+,?)+)\|([0-9]+)\|(.*)\)$");
+        if (match != null && match.Success && match.Groups != null && match.Groups.Count == 4)
         {
-            var idx = match.Groups[1].Captures.Select(c => { int.TryParse(c.Value, CultureInfo.InvariantCulture, out var id); return id; }).ToArray();
-            decimal.TryParse(match.Groups[2].Value, CultureInfo.InvariantCulture, out var pos);
-            return new Position(idx, pos);
+            var nav = match.Groups[1].Value;
+            int.TryParse(match.Groups[2].Value, CultureInfo.InvariantCulture, out var rOrder);
+            decimal.TryParse(match.Groups[3].Value, CultureInfo.InvariantCulture, out var pos);
+            return new Position(nav, rOrder, pos);
         }
 
-        return new Position([0], 0m);
+        return new Position("0",0,0m);
     }
 
     public string ToQueryString()
     {
 
-        return $"({String.Join(',', this.NavigationItemIndex)}|{ScrollPosition.ToString("0.00", CultureInfo.InvariantCulture)})";
+        return $"({NavigationLevel}|{ReadOrder}|{ScrollPosition.ToString("0.00", CultureInfo.InvariantCulture)})";
     }
 }
--- Services/EPubNavigationService.cs
+++ Services/EPubNavigationService.cs
@@ -1,6 +1,9 @@
 using EPubBlazor.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
+using VersOne.Epub;
+using System.Runtime.InteropServices;
 
 namespace EPubBlazor.Services;
 
@@ -9,6 +12,10 @@ public class EPubNavigationService(NavigationManager navigationManager)
     private readonly NavigationManager navigationManager = navigationManager;
 
     public event EventHandler<Position> PositionChanged;
+
+    List<ChapterReadingOrderItem> NavigationTree { get; set; } = [];
+    Dictionary<int, string> PageToLevelDictionary = [];
+
 
     public Position GetPosition()
     {
@@ -19,32 +26,93 @@ public class EPubNavigationService(NavigationManager navigationManager)
     }
 
     public void SetPosition(Position position)
-{
-    string uri = GetUrlForPosition(position);
-    navigationManager.NavigateTo(uri);
-    PositionChanged.Invoke(null,position);
-}
+    {
+        string uri = GetUrlForPosition(position);
+        navigationManager.NavigateTo(uri);
+        PositionChanged.Invoke(null, position);
+    }
 
-public string GetUrlForPosition(Position position)
-{
-    var baseUri = navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
-    return baseUri + $"?p={position.ToQueryString()}";
-}
+    public string GetUrlForPosition(Position position)
+    {
+        var baseUri = navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
+        return baseUri + $"?p={position.ToQueryString()}";
+    }
 
-public void GoToNext()
-{
-    var pos = GetPosition();
-    pos.NavigationItemIndex[pos.NavigationItemIndex.Length-1] +=1;
-    pos.ScrollPosition = 0;
-   SetPosition(pos);
-   
-}
-public void GoToPrevious()
-{
-    var pos = GetPosition();
-    pos.NavigationItemIndex[pos.NavigationItemIndex.Length-1] -=1;
-    pos.ScrollPosition = -1;
-   SetPosition(pos);
-   
-}
+    public void GoToNext()
+    {
+        var pos = GetPosition();
+        // pos.NavigationItemIndex[pos.NavigationItemIndex.Length - 1] += 1;
+        pos.ReadOrder += 1;
+        pos.NavigationLevel = PageToLevelDictionary.ContainsKey(pos.ReadOrder)?  PageToLevelDictionary[pos.ReadOrder] : string.Empty;
+        pos.ScrollPosition = 0;
+        SetPosition(pos);
+
+    }
+    public void GoToPrevious()
+    {
+        var pos = GetPosition();
+        // pos.NavigationItemIndex[pos.NavigationItemIndex.Length - 1] -= 1;
+        pos.ReadOrder -= 1;
+        pos.NavigationLevel = PageToLevelDictionary.ContainsKey(pos.ReadOrder)?  PageToLevelDictionary[pos.ReadOrder] : string.Empty;
+        pos.ScrollPosition = -1;
+        SetPosition(pos);
+
+    }
+
+    public void GenerateNavigationTree(EpubBook book)
+    {
+        NavigationTree = GenerateNavigationTree(book, book.Navigation, string.Empty);
+        PageToLevelDictionary = NavigationTree.SelectMany(n => n.Page, (n, p) =>
+ new { PageId = p, Level = n.Level })
+ .ToDictionary(d => d.PageId, d => d.Level);
+        // PageToLevelDictionary = NavigationTree.SelectMany(n => n.Page, (n, p) =>
+        //  new { PageId = p, Level = n.Level.Split(",").Select(i => int.Parse(i)).ToArray() })
+        //  .ToDictionary(d => d.PageId, d => d.Level);
+    }
+
+    private List<ChapterReadingOrderItem> GenerateNavigationTree(EpubBook book, List<EpubNavigationItem> navigation, string level)
+    {
+        var list = new List<ChapterReadingOrderItem>();
+
+        int j = 0;
+
+
+        for (int i = 0; i < navigation.Count; i++)
+        {
+            var nextLevel = string.IsNullOrEmpty(level) ? i.ToString() : $"{level},{i}";
+
+            var nav1 = navigation[i];
+
+            var nav2 = i + 1 < navigation.Count ? navigation[i + 1] : null;
+
+            List<ChapterReadingOrderItem>? nested = null;
+
+            if (nav1.NestedItems != null && nav1.NestedItems.Count > 0)
+                list.AddRange( GenerateNavigationTree(book, nav1.NestedItems, nextLevel) );
+
+            List<int> roList = new();
+            bool chapterFound = false;
+            for (; j < book.ReadingOrder.Count; j++)
+            // foreach (var ro in book.ReadingOrder)
+            {
+
+                if (book.ReadingOrder[j].FilePath == nav1.Link?.ContentFilePath)
+                    chapterFound = true;
+
+                if (nav2 != null && book.ReadingOrder[j].FilePath == nav2.Link?.ContentFilePath)
+                    break;
+
+                if (chapterFound)
+                    roList.Add(j);
+            }
+
+            list.Add(new ChapterReadingOrderItem(nextLevel, roList.ToArray()));
+        }
+
+        return list;
+
+    }
+
+    public record ChapterReadingOrderItem(string Level, int[] Page);
+
 }
